c8f85f0ef67e104bd7c749234af702913baf6fb1
kaevupoiss <rasmus.talioja@gmail.com>
2020-11-30 20:12:07 +0200

    Kommenteeritud javascript

--- js/index_animation.js
+++ js/index_animation.js
@@ -1,16 +1,27 @@
+// Source: https://www.sitepoint.com/get-url-parameters-with-javascript/
+
+// Võtab terve url-i
 const urlString = window.location.search;
 
+// Võtab url-ist parameetrid
 const urlParameters = new URLSearchParams(urlString);
 
+// Kas parameetrite hulgas on "from":
 if (urlParameters.has('from')) {
+
+    // Võtab milliselt lehelt tuli
     const fromPage = urlParameters.get('from');
 
+    // Võtab elementide puust pildi mille suurust animeerida
     var animateElement = document.getElementsByClassName(fromPage)[0].getElementsByTagName('img')[0];
 
+    // Seab pildile animatsiooni
     animateElement.style.animation = 'scaleAnimation ease-out 1.5s';
     animateElement.style.animationDelay = '0s';
     animateElement.style.animationIterationCount = '1';
     animateElement.style.animationFillMode = 'backwards';
+
+    // Et animatsioon oleks teiste elementide peal
     animateElement.style.position = 'relative';
     animateElement.style.zIndex = '1';
 
--- js/page_change.js
+++ js/page_change.js
@@ -1,10 +1,17 @@
+// Defineerib funktsiooni, läheb html elemendi onclick atribuudi sisse.
 function animateTo(URL, target) {
+
+    // Võtab elementide puust pardi pildi.
     var targetDuck = target.children[0];
 
+    // Seab elemendile stiili
     targetDuck.style.transform = 'scale(200,200)';
+
+    // Seab elemendi teiste peale
     targetDuck.style.position = 'relative';
     targetDuck.style.zIndex = '5'
 
+    // Ootab ära animatsiooni, 1.5 sekundit, ning siis vahetab lehekülge
     setTimeout(
         function () {
             window.location = URL;
